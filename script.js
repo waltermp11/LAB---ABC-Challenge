5d6dc26a38406f244d03ac7589322ebc56055463
waltermp11 <walterm200011@gmail.com>
2026-07-23 15:06:58 -0500

    Botones Funcionales ✅

--- script.js
+++ script.js
@@ -3,11 +3,44 @@ const contador = document.getElementById("contador");
 let numero = 0;
 
 function voltear(card) {
-    // Si la tarjeta NO ha sido volteada todavía
-    if (!card.classList.contains("volteada")) {
-        card.classList.add("volteada");
-        numero++;
-        contador.textContent = numero;
-    } 
-    
+  
+  if (!card.classList.contains("volteada")) {
+    card.classList.add("volteada");
+    numero++;
+    contador.textContent = numero;
+  }
+}
+
+function mostrarVocales() {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    const tipo = card.getAttribute("data-tipo");
+
+    if (tipo === "vocal") {
+      card.style.display = "flex"; 
+    } else {
+      card.style.display = "none"; 
+    }
+  });
+}
+
+function mostrarTodas() {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    card.style.display = "flex"; // Restablece la visibilidad de todas
+  });
+}
+
+function reiniciar() {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    card.classList.remove("volteada");
+    card.style.display = "flex";
+  });
+
+  numero = 0;
+  contador.textContent = numero;
 }
